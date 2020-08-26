64fcdc31efd68c4fe185a753f9dd7692545cba5d
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-08-26 14:26:15 -0500

    ADD: Agrega boton de verificacion

--- js/index.js
+++ js/index.js
@@ -2,24 +2,28 @@ var app = new Vue({
     el: "#app",
     data: {
         conexiones: [],
-        namesSucursales: {nameSuc1: "ZARAGOZA", nameSuc2: "VICTORIA", nameSuc3: "OLUTA", nameSuc4: "BODEGA", nameSuc5: "JALTIPAN"},
         loading: false,
-        urlConexion: "url"
+        /**Datos no publicos */
+        namesSucursales: {nameSuc1: "ZARAGOZA", nameSuc2: "VICTORIA", nameSuc3: "OLUTA", nameSuc4: "BODEGA", nameSuc5: "JALTIPAN"},
+        urlConexion: "uri"
     },
     mounted: function() {
-        this.startLoading();
-        const instancia = this;
-        axios.get(instancia.urlConexion)
-        .then(function (response) {
-            instancia.conexiones = response.data.data;
-            instancia.stopLoading();
-        })
-        .catch(function (error) {
-            instancia.stopLoading();
-            console.log('Error: ' + error);
-        })
+        this.verifyConections();
     },
     methods: {
+        verifyConections: function() {
+            this.startLoading();
+            const instancia = this;
+            axios.get(instancia.urlConexion)
+            .then(function (response) {
+                instancia.conexiones = response.data.data;
+                instancia.stopLoading();
+            })
+            .catch(function (error) {
+                instancia.stopLoading();
+                console.log('Error: ' + error);
+            })
+        },
         loadImage: function (success) {
             const ruta = success ? "./src/success.png" : "./src/warning.jpg";
             return ruta;
